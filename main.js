9d3323077313bc8d249df62c69077c1c40683faa
leo <leoraker@gmail.com>
2022-07-14 22:04:12 -0400

    7-14 changes part 2

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
 //video-ads ytp-ad-module
 
-const evt = new MouseEvent("click", {
-    view: window,
-    bubbles: true,
-    cancelable: true,
-    clientX: 20,
-});
+const evt = new MouseEvent("click", {});
 
 setInterval(() => {
     const adButton = document.getElementsByClassName("ytp-ad-skip-button ytp-button")[0];
+    const bottomAdButton = document.getElementsByClassName("ytp-ad-overlay-close-container")[0];
     if(adButton) {
         adButton.dispatchEvent(evt);
         console.log("Working")
-
+    }
+    if(bottomAdButton) {
+        bottomAdButton.dispatchEvent(evt);
+        console.log("Working")
     }
     console.log("No Button Found")
-}, 1000);
+}, 5);
